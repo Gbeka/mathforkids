74f48d46b609ad99c381d336f1081e00251b10f6
Gbeka <97500383+Gbeka@users.noreply.github.com>
2022-05-11 15:41:59 +0800

    initial commit

---
+++ spa/js/divide.js
@@ -0,0 +1,85 @@
+const option1 = document.getElementById("option1"),
+      option2 = document.getElementById("option2"),
+      option3 = document.getElementById("option3"),
+      audio = document.getElementById("myAudio");  
+      audio1=document.getElementById("right");
+var answer = 0;
+
+function generate_equation(){ 
+    var num1 = Math.floor(Math.random() * 10)+1,
+        num2 = Math.floor(Math.random() * 10)+1,
+        dummyAnswer1 = Math.random() * 10/10,
+        dummyAnswer2 = Math.random() * 10,
+        dummyAnswer1=  dummyAnswer1.toFixed(1);
+        dummyAnswer2= dummyAnswer2.toFixed(1); 
+        allAnswers = [],
+        switchAnswers = [];
+
+
+        if(num1>num2){
+        answer = eval(num1 / num2);
+
+        document.getElementById("num1").innerHTML = num1; 
+        document.getElementById("num2").innerHTML = num2; 
+        }
+        else{
+          answer = eval(num2 / num1);
+
+          document.getElementById("num1").innerHTML = num2; 
+          document.getElementById("num2").innerHTML = num1; 
+          
+        }
+      
+if(Number.isInteger(answer)==false){
+  answer=answer.toFixed(1);
+}
+
+
+
+
+        allAnswers = [answer, dummyAnswer1, dummyAnswer2];
+      
+        for (i = allAnswers.length; i--;){
+          switchAnswers.push(allAnswers.splice(Math.floor(Math.random() * (i + 1)), 1)[0]);
+        };
+      
+        option1.innerHTML = switchAnswers[0];
+        option2.innerHTML = switchAnswers[1];
+        option3.innerHTML = switchAnswers[2]; 
+      };
+
+
+      
+      option1.addEventListener("click", function(){
+          if(option1.innerHTML == answer){
+            generate_equation();
+            audio1.play();
+          }
+          else{
+            audio.play();
+          }
+          
+      });
+
+
+      option2.addEventListener("click", function(){
+        if(option2.innerHTML == answer){
+          generate_equation();
+          audio1.play()
+        }
+        else{
+          audio.play();
+        }
+    });
+    
+    option3.addEventListener("click", function(){
+        if(option3.innerHTML == answer){
+          generate_equation();
+          audio1.play()
+        }
+        else{
+          audio.play();
+        }
+    });
+    
+    generate_equation()
